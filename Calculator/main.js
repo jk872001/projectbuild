3480cd8b31ff229b79a43398c47aa794c5e2a1ab
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-03-03 23:42:03 +0530

    Develop calculator

---
+++ Calculator/main.js
@@ -0,0 +1,17 @@
+const Add = require('./add');
+const Subtract=require('./subtract');
+const Multiply=require('./multiply');
+const Divide=require('./divide');
+
+
+// Addition
+console.log(Add(10,5));
+
+// Subtraction
+console.log(Subtract(10,5));
+
+// Multiplication
+console.log(Multiply(10,5));
+
+// Division
+console.log(Divide(10,5));
